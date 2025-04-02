09e55d2a42ede065826e793b63b217b65e440bb4
Javi Blazquez <j.blazquez.2019@alumnos.urjc.es>
2025-04-02 13:27:37 +0200

    feat: 3.10 done

---
+++ practica03/buscaCadena.js
@@ -0,0 +1,33 @@
+'use strict'
+
+function buscaCadena(strToFind, strBase){
+    let count = 0;
+    let arr = [];
+    if (typeof(strToFind) !== 'string'){
+        throw new Error("El elemento que queremos buscar debe ser una string");
+    }
+    if (typeof(strBase) !== 'string'){
+        throw new Error("El elemento donde buscaremos la cadena debe ser una string");
+    }
+
+    let strLen = strToFind.length
+
+    for (let i = 0; i < strBase.length; i += strLen) {
+        arr.push(strBase.slice(i, i + strLen));
+    }
+    
+    arr.forEach(element => {
+        if (element === strToFind) {
+          count++;
+        }
+      });
+    
+
+    return count
+}
+
+
+console.log(buscaCadena("H","Hola don Pepito, Hola don Jose"));
+console.log(buscaCadena("Ho","Hola, don Pepito, Hola, don Jose"));
+console.log(buscaCadena("Ho","Hola, don Pepito, HOla, don Jose"));
+console.log(buscaCadena("5","125548518195815"));
