186fd8c5e20ad8e2f997c465062ca27ccee0a236
Javi Blazquez <j.blazquez.2019@alumnos.urjc.es>
2025-04-10 11:11:44 +0200

    feat: duplicados working well

---
+++ practica04/duplicados.js
@@ -0,0 +1,28 @@
+'use strict'
+
+let lista = ['tortilla', 'te', 'cafe', 'te', 'tostada','tostada', 'tostada']
+
+
+function duplicados(lista){
+    let sinDuplicados = [];
+
+    for (let product of lista){
+        let found = false
+        
+
+        for (let sublist of sinDuplicados){
+            if (sublist[1] === product){
+                sublist[0] += 1;
+                found =true
+            } 
+            
+        }
+        
+        if(!found) {
+            sinDuplicados.push([1, product])
+        }
+    }
+    console.log(sinDuplicados)
+}
+
+duplicados(lista)
